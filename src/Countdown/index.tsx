731ceb2fdf63aae7c6e732d77f551e7aaf5d0a16
Rafael Veiga <dsg.rafael@gmail.com>
2023-01-28 18:47:03 -0300

    add Countdown widget

---
+++ src/Countdown/index.tsx
@@ -0,0 +1,92 @@
+import React, { useEffect, useState } from "react";
+import styled from "styled-components";
+import dayjs from "dayjs";
+import duration from "dayjs/plugin/duration";
+
+const Countdown = ({ settings: { date, time } }) => {
+	const [days, setDays] = useState(0);
+	const [hours, setHours] = useState(0);
+	const [minutes, setMinutes] = useState(0);
+	const [seconds, setSeconds] = useState(0);
+
+	useEffect(() => {
+		dayjs.extend(duration);
+		const endTime = dayjs(`${date}`);
+		const clockInterval = setInterval(() => {
+			const currentTime = dayjs();
+
+			const daysDiff = endTime.diff(currentTime, "days");
+			const hoursDiff = currentTime.diff(
+				currentTime.endOf("day"),
+				"hours"
+			);
+			const minutesDiff = currentTime.diff(
+				currentTime.endOf("hour"),
+				"minutes"
+			);
+			const secondsDiff = currentTime.diff(
+				currentTime.endOf("minute"),
+				"seconds"
+			);
+
+			setDays(daysDiff);
+			setHours(-hoursDiff);
+			setMinutes(-minutesDiff);
+			setSeconds(-secondsDiff);
+		}, 100);
+
+		() => {
+			clearInterval(clockInterval);
+		};
+	}, []);
+
+	return (
+		<CountdownContainer>
+			<CountdownItem>
+				<h3>{days}</h3>
+				<small>days</small>
+			</CountdownItem>
+			<CountdownItem>
+				<h3>{hours}</h3>
+				<small>hours</small>
+			</CountdownItem>
+			<CountdownItem>
+				<h3>{minutes}</h3>
+				<small>minutes</small>
+			</CountdownItem>
+			<CountdownItem>
+				<h3>{seconds}</h3>
+				<small>seconds</small>
+			</CountdownItem>
+		</CountdownContainer>
+	);
+};
+
+const CountdownContainer = styled.div`
+	display: flex;
+	justify-content: space-between;
+	gap: 10px;
+`;
+
+const CountdownItem = styled.div`
+	text-align: center;
+	border: 1px solid var(--color-accent-1);
+	border-radius: 10px;
+	padding: 20px 0;
+	flex: 1;
+	display: flex;
+	flex-direction: column;
+	gap: 12px;
+
+	h3 {
+		font-size: 50px;
+		line-height: 50px;
+		margin: 0px;
+	}
+
+	small {
+		color: var(--text-muted);
+	}
+`;
+
+export default Countdown;
--- src/Widget.tsx
+++ src/Widget.tsx
@@ -1,5 +1,6 @@
 import * as React from "react";
 import Clock from "./Clock";
+import Countdown from "./Countdown";
 import Quote from "./Quote";
 
 export const Widget = ({ settings }) => {
@@ -9,6 +10,10 @@ export const Widget = ({ settings }) => {
 
 	if (settings.type === "quote") {
 		return <Quote settings={settings} />;
+	}
+
+	if (settings.type === "countdown") {
+		return <Countdown settings={settings} />;
 	}
 
 	return <code>Wrong Settings</code>;
