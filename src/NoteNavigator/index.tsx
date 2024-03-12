e4ff089283412138364fc06c6743f77cf8e5a421
Rafael Veiga <dsg.rafael@gmail.com>
2024-03-11 22:37:47 -0300

    figure out how to open files in href links, start calculating previous and next files

--- src/NoteNavigator/index.tsx
+++ src/NoteNavigator/index.tsx
@@ -1,15 +1,32 @@
+import moment from "moment";
 import { App } from "obsidian";
 import React from "react";
 
 const NoteNavigator = ({ app }: NoteNavigatorProps) => {
-	console.log(app.workspace.getActiveFile());
-	const { basename } = app.workspace.getActiveFile();
+	const openFile = app.workspace.getActiveFile();
+
+	if (!openFile?.basename)
+		return (
+			<div className="NoteNavigator_Container">
+				<strong>No open file found</strong>
+			</div>
+		);
+
+	const openFileDate = moment(openFile.basename).format("YYYY-MM-DD");
+	const previousDay = moment(openFile.basename)
+		.subtract(1, "days")
+		.format("YYYY-MM-DD");
+	const nextDay = moment(openFile.basename)
+		.add(1, "days")
+		.format("YYYY-MM-DD");
+
+	console.log(openFileDate, previousDay, nextDay);
 
 	return (
 		<div className="NoteNavigator_Container">
-			<a href="#">⬅️ 2023-01-09</a>
-			<strong>{basename}</strong>
-			<a href="#">2023-01-11 ➡️</a>
+			<a href="obsidian://open?file=file">⬅️ {previousDay}</a>
+			<strong>{openFile.basename}</strong>
+			<a href="#">{nextDay} ➡️</a>
 		</div>
 	);
 };
