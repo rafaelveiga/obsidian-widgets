05ff78e7358f6d1d6fe5a0a4fd63e73094fa5022
Rafael Veiga <dsg.rafael@gmail.com>
2024-03-11 22:09:40 -0300

    init note navigator widget

---
+++ src/NoteNavigator/index.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const NoteNavigator = () => {
+	return <div className="NoteNavigator_Container">lorem10</div>;
+};
+
+export interface NoteNavigatorSettings {
+	type: "navigator";
+}
+
+export default NoteNavigator;
